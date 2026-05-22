6034a5cee44dcf082c1693db8edd7f94870dc058
Jirka <tuma.rsrobot@gmail.com>
2026-05-22 13:22:36 +0200

    Archive repository after move

--- extension/options/options.js
+++
@@ -1,9 +0,0 @@
-const sel = document.getElementById('quality');
-
-chrome.storage.sync.get({ quality: 'high' }, ({ quality }) => {
-  sel.value = quality;
-});
-
-sel.addEventListener('change', () => {
-  chrome.storage.sync.set({ quality: sel.value });
-});
